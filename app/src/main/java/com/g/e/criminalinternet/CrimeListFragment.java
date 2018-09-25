0795a00d4af8c828823e7b302544c58ae11d188f
alena <49859442+alena-dev@users.noreply.github.com>
2018-09-25 14:17:05 -0700

    Task-2. Implement RecycleView

--- app/src/main/java/com/g/e/criminalinternet/CrimeListFragment.java
+++ app/src/main/java/com/g/e/criminalinternet/CrimeListFragment.java
@@ -1,7 +1,31 @@
 package com.g.e.criminalinternet;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeListFragment extends Fragment {
 
+    private RecyclerView mCrimeRecycleView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_crime_list,
+                container, false);
+        mCrimeRecycleView=(RecyclerView)view
+                .findViewById(R.id.crime_recycler_view);
+        mCrimeRecycleView.setLayoutManager(new LinearLayoutManager
+                (getActivity()));
+        
+        return view;
+    }
 }
