a8371f00aca9f1cc61e0ef8e17e7dbcd6016a4b2
alena <49859442+alena-dev@users.noreply.github.com>
2018-09-26 17:41:10 -0700

    Task-4. Add extra from Crime to CrimeFragment

--- app/src/main/java/com/g/e/criminalinternet/controller/CrimeActivity.java
+++ app/src/main/java/com/g/e/criminalinternet/controller/CrimeActivity.java
@@ -1,6 +1,21 @@
 package com.g.e.criminalinternet.controller;
 
+import android.content.Context;
+import android.content.Intent;
+
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+
+    public static final String EXTRA_CRIME_ID=
+            "com.g.e.criminalinternet.model.crime_id";
+
+    public static Intent createIntent(Context packageContext, UUID crimeId){
+        Intent intent=new Intent(packageContext,CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
 
     @Override
     protected android.support.v4.app.Fragment createFragment(){
--- app/src/main/java/com/g/e/criminalinternet/controller/CrimeListFragment.java
+++ app/src/main/java/com/g/e/criminalinternet/controller/CrimeListFragment.java
@@ -75,7 +75,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view){
-            Intent intent =new Intent(getActivity(), CrimeActivity.class);
+            Intent intent =CrimeActivity.createIntent(getActivity(),mCrime.getId());
             startActivity(intent);
         }
 
