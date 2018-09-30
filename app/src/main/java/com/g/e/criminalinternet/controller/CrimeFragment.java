2bdb13d3cddc3d009e2500dd3691d6f75618fdfc
alena <49859442+alena-dev@users.noreply.github.com>
2018-09-30 04:20:54 -0700

    Task-6. Create and show DialogFragment

--- app/src/main/java/com/g/e/criminalinternet/controller/CrimeFragment.java
+++ app/src/main/java/com/g/e/criminalinternet/controller/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -24,6 +25,7 @@ public class CrimeFragment extends Fragment {
 
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE ="DialogDate";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -73,7 +75,14 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = (Button) view.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox =(CheckBox)view.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
