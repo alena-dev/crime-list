819582beac3ee3d64d327d6cc84c2ec7504b60d0
alena <49859442+alena-dev@users.noreply.github.com>
2018-09-30 12:01:32 -0700

    Task-5. Refactor

--- app/src/main/java/com/g/e/criminalinternet/controller/CrimeListFragment.java
+++ app/src/main/java/com/g/e/criminalinternet/controller/CrimeListFragment.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.g.e.criminalinternet.R;
 import com.g.e.criminalinternet.model.Crime;
@@ -89,7 +88,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view){
-            Intent intent =CrimePagerActivity.newIntent(getActivity(),mCrime.getId());
+            Intent intent =CrimePagerActivity.createIntent(getActivity(),mCrime.getId());
             startActivity(intent);
         }
 
--- app/src/main/java/com/g/e/criminalinternet/controller/CrimePagerActivity.java
+++ app/src/main/java/com/g/e/criminalinternet/controller/CrimePagerActivity.java
@@ -23,7 +23,7 @@ public class CrimePagerActivity extends AppCompatActivity{
     private ViewPager mViewPager;
     private CrimeLab mCrimeLab;
 
-    public static Intent newIntent(Context packageContext, UUID crimeId){
+    public static Intent createIntent(Context packageContext, UUID crimeId){
         Intent intent= new Intent(packageContext, CrimePagerActivity.class);
         intent.putExtra(EXTRA_CRIME_ID, crimeId);
         return intent;
