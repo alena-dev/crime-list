450f03c04463d26bb2d47af87e9ca94200a5e5d6
alena <49859442+alena-dev@users.noreply.github.com>
2018-10-01 18:34:26 -0700

    Task-6-2. Override onCreateView instead onCreateDialog (DatePickerFragment)

--- app/src/main/java/com/g/e/criminalinternet/controller/DatePickerFragment.java
+++ app/src/main/java/com/g/e/criminalinternet/controller/DatePickerFragment.java
@@ -6,10 +6,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import com.g.e.criminalinternet.R;
@@ -24,6 +27,7 @@ public class DatePickerFragment extends DialogFragment {
     public static final String EXTRA_DATE = "com.g.e.criminalinternet.date";
 
     private DatePicker mDatePicker;
+    private Button mPositiveButton;
 
     public static DatePickerFragment createInstance(Date date){
         Bundle args=new Bundle();
@@ -34,11 +38,12 @@ public class DatePickerFragment extends DialogFragment {
         return fragment;
     }
 
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-       Date date = (Date)getArguments().getSerializable(ARG_DATE);
+    @ Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
 
+        Date date = (Date)getArguments().getSerializable(ARG_DATE);
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         int year = calendar.get(Calendar.YEAR);
@@ -46,28 +51,27 @@ public class DatePickerFragment extends DialogFragment {
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         View view = LayoutInflater.from(getActivity())
-                .inflate(R.layout.dialog_date, null);
+                .inflate(R.layout.dialog_date_fragment, null);
 
-        mDatePicker = (DatePicker)view
-                .findViewById(R.id.dialog_date_picker);
+        mDatePicker = (DatePicker)view.findViewById(R.id.dialog_date_picker);
         mDatePicker.init(year, month, day, null);
 
-        return new AlertDialog.Builder(getActivity())
-                .setView(view)
-                .setTitle(R.string.date_picker_title)
-                .setPositiveButton(android.R.string.ok,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                int year = mDatePicker.getYear();
-                                int month = mDatePicker.getMonth();
-                                int day = mDatePicker.getDayOfMonth();
-                                Date date = new GregorianCalendar(year, month, day)
-                                        .getTime();
-                                sendResult(Activity.RESULT_OK, date);
-                            }
-                        })
-                .create();
+        mPositiveButton = (Button)view.findViewById(R.id.dialog_date_positive_button);
+        mPositiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedYear = mDatePicker.getYear();
+                int selectedMonth = mDatePicker.getMonth();
+                int selectedDayOfMonth = mDatePicker.getDayOfMonth();
+                Date date = new GregorianCalendar(selectedYear, selectedMonth, selectedDayOfMonth)
+                        .getTime();
+                sendResult(Activity.RESULT_OK, date);
+                getActivity().getSupportFragmentManager().beginTransaction().remove(DatePickerFragment.this).commit();
+            }
+        });
+
+
+        return view;
     }
 
     public void sendResult(int resultCode, Date date){
