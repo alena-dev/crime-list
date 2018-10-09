2f07176dcb95227c301dfa5281b1f2e6a1c9e1b6
alena <49859442+alena-dev@users.noreply.github.com>
2018-10-09 22:17:43 +0300

    Task-11. Create layout with two fragment containers

--- app/src/main/java/com/g/e/criminalinternet/controller/CrimeListActivity.java
+++ app/src/main/java/com/g/e/criminalinternet/controller/CrimeListActivity.java
@@ -1,9 +1,16 @@
 package com.g.e.criminalinternet.controller;
 
+import com.g.e.criminalinternet.R;
+
 public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected android.support.v4.app.Fragment createFragment(){
         return new CrimeListFragment();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_twopane;
     }
 }
